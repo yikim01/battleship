4fbadf3c164b085ea7d278a17a380e2adee46d3b
Kimberly Yi <yikim01@gmail.com>
2015-07-06 18:16:14 -0700

    changed to battleship with html and some CSS

---
+++ README.md
@@ -0,0 +1,8 @@
+## Mario Memory Game
+
+# test your memory with with your favorite character from Mario Kart
+
+#OBj
+* match as many of the characters as you can
+
+#How to play
---
+++ js/main.js
@@ -0,0 +1,37 @@
+// console.log('activate framework!');
+
+// var randomLoc = Math.floor(Math.random() * 6);
+// var location1 = randomLoc;
+// var location2 = location1 + 1;
+// var location3 = location2 + 1;
+
+// var guess;
+// var hits = 0;
+// var guesses = 0;
+// var isSunk = false;
+
+// //Variable declaration go here
+
+// while (isSunk === false) {
+//   guess = prompt("Ready, aim, fire!(enter a number 0-7):");
+//   if (guess < 0 || guess > 7) {
+//     alert("Please enter a valid cell number!");
+//   } else {
+//     guesses = guesses + 1;
+
+//     if (guess == location1 || guess == location2 || guess == location3) {
+//       hits = hits + 1;
+//       alert("HIT!");
+
+//       if (hits == 3) {
+//         isSunk = true;
+//         alert("You sank my battleship!");
+//       }
+//     } else {
+//       alert("MISS!");
+//     }
+//   }
+// }
+// var stats = "You took " + guesses + " guesses to sink the battleship, " +
+//             "which means your shooting accuracy was " + (3/guesses);
+// alert(stats);
