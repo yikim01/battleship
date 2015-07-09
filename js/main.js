15fd63efffd60e3a294054d1c6303b96a4e428ff
Kimberly Yi <yikim01@gmail.com>
2015-07-09 09:19:56 -0700

    cleaned the first function

--- js/main.js
+++ js/main.js
@@ -34,17 +34,19 @@ var row = [
   ['null', 'null', 'null', 'null', 'null', 'null', 'null', 'null', 'null', 'null'],
   ['null', 'null', 'null', 'null', 'null', 'null', 'null', 'null', 'null', 'null'],
 ];
-
+//have counter for the hits once 0 is reach "you sunk my ship"
 var ships = [
   {location: ["10","20"],
-  hits: ["",""]},
+  hits_left: 2},
   {location: ["33", "34", "35"],
-  hits :["","","",]},
+  hits_left: 3},
   {location: ["62", "72", "82", "92"],
-  hits :["","","",""]},
+  hits_left: 4},
   {location: ["85", "86", "87", "88", "89"],
-  hits :["","","","",""]},
+  hits_left: 5},
 ];
+
+var positions = [10, 20, 33, 34];
 
 var guess;
 var guesses = 0;
@@ -56,42 +58,46 @@ var isSunk = false;
 //tie in guess with a click eventlistener from above after the function guess
 //function: show if it is a hit miss click again or sunk
 
-var ships = function(guess) {
+// var ships = function(guess) {
 
+// }
 
-  var cell = document.getElementsByClassName("cell");
-  var clickEvent = function(){
+var cells = document.getElementsByClassName("cell");
 
-  for (var i = 0; i < cell.length; i++) {
-    cell[i].addEventListener('click', function(){
-      console.log(this);
-  });
+for (var i = 0; i < cells.length; i++) {
+  cells[i].addEventListener('click', function(){
+    //if location of ship is clicked "hit"
+    //for (var ships[i] = 0; i < ships.length; i++) {
+      if (positions.indexOf(parseInt (this.id)) > -1){
+        console.log("Hit me!");
+      } else {
+        console.log("Missed me!")
+      }
+      //else "miss"
+      //loop through  the ships to match locations
+    console.log(positions, this.id);
+  //}
+});
 
-  for(var i = 0; i < clickEvent; i++) {
-    // var ship should be ships A B C D
-    var ships = this.ships[i];
-    // locations of ships A B C D
-    var location = ships.locations;
-    var index = ship.locations.indexOf(guess);
-    if (cell === location) {
-      ships.hits[index] = "hit";
-      view.displayHit(guess);
-      view.displayMessage("HIT!");
-      console.log("HIT!");
-      return true;
-      //if the user doesnt click where ships are located this will happen
-    } else
-    view.displayMiss(guess);
-    view.displayMessage("You missed muhahaha!");
-    return false;
-    }
-  }
-
-
-    };
-  }
-
-
+  // for(var i = 0; i < clickEvent; i++) {
+  //   // var ship should be ships A B C D
+  //   var ships = this.ships[i];
+  //   // locations of ships A B C D
+  //   var location = ships.locations;
+  //   var index = ship.locations.indexOf(guess);
+  //   if (cell === location) {
+  //     ships.hits[index] = "hit";
+  //     view.displayHit(guess);
+  //     view.displayMessage("HIT!");
+  //     console.log("HIT!");
+  //     return true;
+  //     //if the user doesnt click where ships are located this will happen
+  //   } else
+  //   view.displayMiss(guess);
+  //   view.displayMessage("You missed muhahaha!");
+  //   return false;
+  // }
+}
 
 // After eventlistener click is made
 // pass the div id that was clicked on and check against the
